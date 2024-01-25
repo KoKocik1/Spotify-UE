9d031b3572741a10cf1bf4c82aa5df11b4fe6003
Belkius <118485347+Belkius@users.noreply.github.com>
2024-01-25 19:17:16 +0100

    Added job scheduling and docker files

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
+import schedule
+import time
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import dotenv_values
 from supabase import create_client, Client
 
-secrets = dotenv_values('secret.env')
+def job():
+        secrets = dotenv_values('secret.env')
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=secrets['CLIENT_ID'],
-                                                           client_secret=secrets['CLIENT_SECRET']))
+        sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=secrets['CLIENT_ID'],
+                                                                client_secret=secrets['CLIENT_SECRET']))
 
-results = sp.playlist_items(playlist_id='37i9dQZEVXbN6itCcaL3Tt', limit=10, market='PL',)
+        results = sp.playlist_items(playlist_id='37i9dQZEVXbN6itCcaL3Tt', limit=10, market='PL',)
 
-supabase: Client = create_client(secrets['DATABASE_URL'], secrets['DATABASE_KEY'])
+        supabase: Client = create_client(secrets['DATABASE_URL'], secrets['DATABASE_KEY'])
 
-position=0
+        position=0
 
-for item in results['items']:
+        for item in results['items']:
 
-        item=item['track']
-        album=item['album']
-        arists=item['artists']
+                item=item['track']
+                album=item['album']
+                arists=item['artists']
 
-        position=position+1
+                position=position+1
 
-        artistData=[]
-        for artist in arists:
-                artistData.append(artist['name'])
+                artistData=[]
+                for artist in arists:
+                        artistData.append(artist['name'])
 
-        while(len(artistData)<4):
-             artistData.append("")
+                while(len(artistData)<4):
+                        artistData.append("")
         
-        data = {
-        "position": position,
-        "song_name": item['name'],
-        "duration": item['duration_ms'],
-        "explicit": item['explicit'],
-        "popularity": item['popularity'],
-        "album_type": album['album_type'],
-        "album_name": album['name'],
-        "spotify_id": item['id'],
-        "artist_1": artistData[0],
-        "artist_2": artistData[1],
-        "artist_3": artistData[2],
-        "artist_4": artistData[3]
-        }
+                data = {
+                "position": position,
+                "song_name": item['name'],
+                "duration": item['duration_ms'],
+                "explicit": item['explicit'],
+                "popularity": item['popularity'],
+                "album_type": album['album_type'],
+                "album_name": album['name'],
+                "spotify_id": item['id'],
+                "artist_1": artistData[0],
+                "artist_2": artistData[1],
+                "artist_3": artistData[2],
+                "artist_4": artistData[3]
+                }
         
-        supabase.table('Top50Poland').insert(data).execute()
+                supabase.table('Top50Poland').insert(data).execute()
+# Run the job once initially
+job()
+
+# Schedule the job to run every 24 hours
+schedule.every(24).hours.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)  # Sleep to avoid excessive CPU usage
